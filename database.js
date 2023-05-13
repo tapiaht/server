cc45297e97fa5dd97c9fd32ab038dd29a59e7052
tapiaht <hiver.tapia@uab.edu.bo>
2023-05-13 01:10:31 -0400

    migrando a cloudgoogle

--- database.js
+++ database.js
@@ -10,122 +10,52 @@ const pool=mysql
 
 })
 .promise();
-
+//USERS CRUD
 export async function getUserByID(id) {
     const [rows] = await pool.query(`SELECT * FROM user WHERE id = ?`, [id]);
     return rows[0];
   }
-  export async function getTodosByUserIdDate(userId,daytime) {
-      // const [rows] = await pool.query(`SELECT * FROM todos WHERE DATE_FORMAT(daytime, '%Y-%m-%d') = ?`,[daytime]);
-    const [rows] = await pool.query(`SELECT * FROM todos WHERE user_id = ? AND DATE(inday) = ?`,[userId,daytime]);
+  export async function getUsers() {
+    const [rows] = await pool.query("SELECT * FROM user");
     return rows;
-  }
-  // SELECT * FROM todos WHERE user_id=2 and date(?)=date(daytime)
+  }  
 export async function getUserByEmail(email) {
     const [rows] = await pool.query(`SELECT * FROM user WHERE email = ?`, [email]);
     return rows[0];
 }
-  export async function getTodosByID(id) {
-    const [rows] = await pool.query(
-      `
-      SELECT todos.*, shared_todos.shared_with_id
-      FROM todos
-      LEFT JOIN shared_todos ON todos.id = shared_todos.todo_id
-      WHERE todos.user_id = ? OR shared_todos.shared_with_id = ?
-    `,
-      [id, id]
-    );
-    return rows;
-  }
-  export async function getTodoById(id){
+
+export async function createUser(name,email) {
+  const [result] = await pool.query(
+    `
+    INSERT INTO user (name, email)
+    VALUES (?, ?)
+  `,
+    [name, email]
+  );
+  const userID = result.insertId;
+  return getUserByID(userID);
+}
+
+//TODOS CRUD
+  export async function getTodosById(id){
     const [row]= await pool.query(`SELECT * FROM todos WHERE id=?`, [id]);
     return row[0];
 }
-export async function getTodo(id) {
-    const [rows] = await pool.query(`SELECT * FROM todos WHERE id = ?`, [id]);
-    return rows[0];
-  }
   export async function getTodos() {
     const [rows] = await pool.query("SELECT * FROM todos");
     return rows;
   }
-  export async function getUsers() {
-    const [rows] = await pool.query("SELECT * FROM user");
-    return rows;
-  }
-
-  export async function createUser(name,email) {
+  export async function createTodo( title, type, advice, intime) {
     const [result] = await pool.query(
       `
-      INSERT INTO user (name, email)
-      VALUES (?, ?)
+      INSERT INTO todos (title, type, advice, intime)
+      VALUES (?, ?, ?, ?)
     `,
-      [name, email]
-    );
-    const userID = result.insertId;
-    return getUserByID(userID);
-  }
-  export async function createTodo(user_id, title) {
-    const [result] = await pool.query(
-      `
-      INSERT INTO todos (user_id, title)
-      VALUES (?, ?)
-    `,
-      [user_id, title]
+      [title, type, advice, intime]
     );
     const todoID = result.insertId;
-    return getTodo(todoID);
+    return getTodosById(todoID);
   }
-
-  export async function create8m(user_id, day) {
-    try {
-    const [result] = await pool.query(
-      `
-INSERT INTO todos (title, user_id,inday,intime) 
-VALUES 
-("💪Ejercicio 1er",?,?,"07:30:00"),
-("💧Agua 1er Vaso",?,?,"07:30:00"),
-("💧Agua 2do Vaso",?,?,"08:00:00"),
-("🍏Nutricion Desayuno",?,?,"08:00:00"),
-("💧Agua 3er Vaso",?,?,"11:30:00"),
-("🍲Nutricion Almuerzo",?,?,"12:00:00"),
-("💪Ejercicio 2do",?,?,"19:00:00"),
-("💧Agua 4to Vaso",?,?,"19:30:00"),
-("🍜Nutricion Cena",?,?,"20:00:00"),
-("🛌Descanso",?,?,"22:00:00");
-    `,
-    [
-      user_id,
-      day,
-      user_id,
-      day,
-      user_id,
-      day,
-      user_id,
-      day,
-      user_id,
-      day,
-      user_id,
-      day,
-      user_id,
-      day,
-      user_id,
-      day,
-      user_id,
-      day,
-      user_id,
-      day,
-    ]
-    );
-    const todoID = result.insertId;
-    return getTodo(todoID);
-    // return result;
-  } catch (error) {
-    console.error(error);
-    throw new Error("Error creating 8m");
-  }
-  }
-
   export async function deleteTodo(id) {
     const [result] = await pool.query(
       `
@@ -135,11 +65,45 @@ VALUES
     );
     return result;
   }
+
+//CHALLENGE CRUD
+export async function createChallenge(todo_id, user_id, inday) {
+  try {
+  const [result] = await pool.query(
+    `
+INSERT INTO challenge (todo_id, user_id, inday) 
+VALUES (?,?,?);
+  `,
+  [ todo_id, user_id, inday]
+  );
+  const challengeID = result.insertId;
+  return getChallengeByID(challengeID);
+} catch (error) {
+  console.error(error);
+  throw new Error("Error creating challenge");
+}
+}
+export async function getChallenge() {
+  const [rows] = await pool.query(`SELECT * FROM challenge`);
+  return rows;
+}
+export async function getChallengeByID(id) {
+  const [rows] = await pool.query(`SELECT * FROM challenge WHERE id = ?`,[id]);
+  return rows[0];
+}
+export async function getChallengeByUserId(user_id) {
+const [rows] = await pool.query(`SELECT * FROM challenge WHERE user_id = ?`,[user_id]);
+return rows;
+}
+export async function getChallengeByUserIdDate(userId,daytime) {
+  const [rows] = await pool.query(`SELECT * FROM challenge WHERE user_id = ? AND DATE(inday) = ?`,[userId,daytime]);
+  return rows;
+}
   export async function toggleCompleted(id, value) {
     const newValue = value === true ? "TRUE" : "FALSE";
     const [result] = await pool.query(
       `
-      UPDATE todos
+      UPDATE challenge
       SET completed = ${newValue} 
       WHERE id = ?;
       `,
@@ -147,7 +111,7 @@ VALUES
     );
     return result;
   }
-
+//SHARE
   export async function getSharedTodoByID(id) {
     const [rows] = await pool.query(
       `SELECT * FROM shared_todos WHERE todo_id = ?`,
@@ -166,3 +130,16 @@ VALUES
     );
     return result.insertId;
   }
+
+  // export async function getTodosByID(id) {
+  //   const [rows] = await pool.query(
+  //     `
+  //     SELECT todos.*, shared_todos.shared_with_id
+  //     FROM todos
+  //     LEFT JOIN shared_todos ON todos.id = shared_todos.todo_id
+  //     WHERE todos.user_id = ? OR shared_todos.shared_with_id = ?
+  //   `,
+  //     [id, id]
+  //   );
+  //   return rows;
+  // }
---
+++ server.js
@@ -0,0 +1,153 @@
+import express from  'express';
+import {
+    getTodos,
+    getUsers,
+    shareTodo,
+    deleteTodo,
+    getTodosById,
+    createTodo,
+    createUser,
+    toggleCompleted,
+    getUserByEmail,
+    getUserByID,
+    create8m,
+    getTodosByUserIdDate,
+    getTodosByUserId,
+    getSharedTodoByID,
+} from "./database.js";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const corsOptions = {
+    // origin: "http://127.0.0.1:5173", // specify the allowed origin
+    origin: "https://apiexpress-hu67.onrender.com:5173", // specify the allowed origin
+    methods: ["POST", "GET"], // specify the allowed methods
+    credentials: true, // allow sending credentials (cookies, authentication)
+  };
+  const developers = [
+    { id: 1, name: "Beto", apiKey: "abcdef123456" },
+    { id: 2, name: "Pedro", apiKey: "ghijkl789012" },
+  ];
+  const ckeckApiKey = (req, res, next) => {
+    const apiKey = req.headers["x-api-key"];
+    const developer = developers.find((d) => d.apiKey === apiKey); //check if we have a dev with that key
+    if (!developer) {
+      return res.status(401).json({ message: "Unauthorized, invalid Api Key" });
+    }
+    req.developer = developer;
+    next();
+  };
+
+const app=express();
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cors(corsOptions));
+// app.use(ckeckApiKey);
+
+app.get("/todos/:id",async (req,res)=>{
+  
+    const todos=await getTodosById(req.params.id);
+    if (!todos) {
+      return res.status(404).send({ message: "TOdo not found" });
+    }
+    res.status(200).send(todos);
+})
+
+app.get("/todos/shared_todos/:id", async (req, res) => {
+    const todo = await getSharedTodoByID(req.params.id);
+    const author = await getUserByID(todo.user_id);
+    const shared_with = await getUserByID(todo.shared_with_id);
+    res.status(200).send({ author, shared_with });
+  });
+  app.get("/users", async (req, res) => {
+    try {
+    const users = await getUsers();
+    res.status(200).send(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: 'No se pudo obtener la información de los usuarios' });
+  }
+  });
+  app.get("/users/:id", async (req, res) => {
+    const user = await getUserByID(req.params.id);
+    res.status(200).send(user);
+  });
+  app.get("/todos/",async (req,res)=>{
+    const todos=await getTodos();
+    res.status(200).send(todos);
+  });
+
+  app.get("/todos/:userId/:daytime", async (req, res) => {
+    try {
+      const todo = await getTodosByUserIdDate(req.params.userId, req.params.daytime);
+      res.status(200).send(todo);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Error al obtener los todos");
+    }
+  });
+  app.get("/todosuser/:user_id", async (req, res) => {
+    try {
+      const todo = await getTodosByUserId(req.params.user_id);
+      res.status(200).send(todo);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Error al obtener las tareas del usuario");
+    }
+  });
+  app.get("/user/:email", async (req, res) => {
+    const user = await getUserByEmail(req.params.email);
+  if (!user) {
+    return res.status(404).send({ message: "User not found" });
+  }
+  res.status(200).send(user);
+  });
+  app.put("/todos/:id", async (req, res) => {
+    const { value } = req.body;
+    const todo = await toggleCompleted(req.params.id, value);
+    res.status(200).send(todo);
+  });
+  
+  app.delete("/todos/:id", async (req, res) => {
+    await deleteTodo(req.params.id);
+    res.send({ message: "Todo deleted successfully" });
+  });
+  
+  app.post("/todos/shared_todos", async (req, res) => {
+    const { todo_id, user_id, email } = req.body;
+    // const { todo_id, user_id, shared_with_id } = req.body;
+    const userToShare = await getUserByEmail(email);
+    const sharedTodo = await shareTodo(todo_id, user_id, userToShare.id);
+    res.status(201).send(sharedTodo);
+  });
+  app.post("/todos", async (req, res) => {
+    const { user_id, title } = req.body;
+    const todo = await createTodo(user_id, title);
+    res.status(201).send(todo);
+  });
+  app.post("/create8m", async (req, res) => {
+    const { user_id, day } = req.body;
+    try {
+      const todo = await create8m(user_id, day);
+      res.status(201).send(todo);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ message: "Error creating 8map" });
+    }
+  });
+  app.post("/users", async (req, res) => {
+    const { name, email } = req.body;
+    const user = await createUser(name, email);
+    res.status(201).send(user);
+  });
+
+
+app.get('/', (req, res) => {
+  res.send('Hello from App Engine!');
+});
+
+// Listen to the App Engine-specified port, or 8080 otherwise
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
